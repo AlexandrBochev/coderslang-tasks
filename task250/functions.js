2dcb58029e8fed0373489c7edd80c89fd71a393e
Alexandr Bochev <abochev@gmail.com>
2022-08-28 18:51:30 +0300

    task250-6 done

--- task250/functions.js
+++ task250/functions.js
@@ -1,3 +1,6 @@
 export const init = (term) => {
-  term('Welcome to the mining game!')
+  term('Welcome to the mining game!');
+  term.clear();
+  term.hideCursor();
+  term.grabInput();
 };
--- task250/gameEngine.js
+++ task250/gameEngine.js
@@ -1,5 +1,8 @@
 import { init } from './functions.js'
+import { handleKeyPress } from "./handlers.js";
 
-export const startMiningGame = (term) => {
-  init(term)
+export const startMiningGame = (term, state) => {
+  init(term);
+  const handler = handleKeyPress(term, state);
+  term.on('key', handler)
 };
