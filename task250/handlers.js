b1d30cb9947415fd8107c58eb5fe36f0d0277d60
Alexandr Bochev <abochev@gmail.com>
2022-08-28 17:49:29 +0300

    task250-5 done

--- task250/handlers.js
+++ task250/handlers.js
@@ -1,6 +1,9 @@
 export const handleKeyPress = (term, state) => {
   const gameTable = (name, matches, data) => {
-    
-  };
+    let key = String.fromCharCode(data.code);
+    if (key === 'g' || key === 'G') {
+      state.gold ++;
+    }
+  }
   return gameTable
 }
--- task250/solution.js
+++ task250/solution.js
@@ -1,6 +1,5 @@
 // LET'S DIG SOME GOLD!
 import { startMiningGame } from './gameEngine.js'
-import { term } from './constants.js'
-import { config } from "./constants.js";
+import { term, config } from './constants.js'
 
 startMiningGame(term, config)
