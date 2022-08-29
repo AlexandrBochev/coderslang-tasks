7fae44235e9be4ccb25eb306279c8bf24911cd73
Alexandr Bochev <abochev@gmail.com>
2022-08-29 13:02:35 +0300

    task250-9 done

--- task250/handlers.js
+++ task250/handlers.js
@@ -1,9 +1,19 @@
 export const handleKeyPress = (term, state) => {
-  const gameTable = (name, matches, data) => {
+  return function gameTable(name, matches, data) {
+
     let key = String.fromCharCode(data.code);
     if (key === 'g' || key === 'G') {
       state.gold ++;
+    } else {
+
+      for (let i = 0; i < state.producers.length; i++) {
+        if (key === String(state.producers[i].id)) {
+          state.gold -= state.producers[i].cost;
+          state.producers[i].cost *= state.producers[i].growthRate;
+          state.producers[i].count++;
+        }
+      }
     }
+
   }
-  return gameTable
 }
