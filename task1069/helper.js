e2a7cb2e1fcf3a2e17784b087c8f03dafb5f6ebb
Alexandr Bochev <abochev@gmail.com>
2022-09-13 14:43:54 +0300

    HTML done

--- task1069/helper.js
+++ task1069/helper.js
@@ -1,47 +1,57 @@
 export const formatNumber = (n) => {
 
-  const k = 1000;
-  const m = k * k;
-  const b = m * k;
-  const t = b * k;
-  const aa = t * k;
-  const ab = aa * k;
-  const ac = ab * k;
+  const K = 1000;
+  const M = 1000000;
+  const B = 1000000000;
+  const T = 1000000000000;
+  let res = 0;
 
-  console.log(BigInt(ac));
+  const exhibitor = Math.pow(10, n.toString().length - 1);
+  const resultNumber = (n / exhibitor).toFixed(2);
 
-  if (n < k) {
-    return n.toFixed(1);
-  } else  if (n >= k && n < m) {
-    return (n / k).toFixed(2) + 'K'
-  } else  if (n >= m && n < b) {
-    return (n / m).toFixed(2) + 'M'
-  } else  if (n >= b && n < t) {
-    return (n / b).toFixed(2) + 'B'
-  } else  if (n >= t && n < aa) {
-    return (n / t).toFixed(2) + 'T'
-  } else  if (n >= aa && n < ab) {
-    return BigInt((n / aa).toFixed(2) + 'aa')
+  const numberForSuffix = () => {
+    let test = (n / 1000).toFixed(0);
+    let i = -1;
+    while (test > 0) {
+      ++i;
+      test = (test / 1000).toFixed(0);
+    }
+    return i;
   }
 
+  const getSuffix = (num) => {
 
-  // if ( n < 1000 ) {
-  //   return n.toFixed(1);
-  // }
-  // if ( (n >= 1000) && (n < 1000000) ) {
-  //     return (n/1000).toFixed(2) + 'K';
-  // }
-  // if ( (n >= 1000000) && (n < 1000000000)) {
-  //     return (n/1000000).toFixed(2) + 'M';
-  // }
-  // if ( (n >= 1000000000) && (n < 1000000000000)) {
-  //     return (n/1000000000).toFixed(2) + 'B';
-  // }
-  // if (n >= 1000000000000) {
-  //     return (n/1000000000000).toFixed(2) + 'T';
-  // }
+    const arrayChar = [];
+    const numChar = (num / 26).toFixed(0);
+    const nChar = num % 26;
+
+    for (let i = 97; i <= 122; i++) {
+      arrayChar.push(String.fromCharCode(i));
+    }
+    
+    return arrayChar[numChar] + arrayChar[nChar];
+    
+  }
+
+  if (n > T * 10) {
+    return resultNumber + getSuffix(numberForSuffix() - 4);
+  } else if (n >= T) {
+    res = n / T;
+    return res.toFixed(2) + 'T';
+  } else if (n >= B) {
+    res = n / B;
+    return res.toFixed(2) + 'B';
+  } else if (n >= M) {
+    res = n / M;
+    return res.toFixed(2) + 'M';
+  } else if (n >= K) {
+    res = n / K;
+    return res.toFixed(2) + 'K';
+  } else {
+    return n.toFixed(1);
+  }
 
 }
 
-formatNumber()
+
 
--- task1069/solution.js
+++ task1069/solution.js
@@ -1,6 +1,6 @@
 import { formatNumber } from './helper.js';
 
-const numbers = [100000000000000000n];
+const numbers = [1000021000000000]
 
 for (let i = 0; i < numbers.length; i++) {
   console.log(`Formatted ${numbers[i]} looks like ${formatNumber(numbers[i])}`);
