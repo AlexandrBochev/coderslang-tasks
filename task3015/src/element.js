1e6b32b5a03b8dcf6342bf30a71a9111d2a9e5e8
Alexandr Bochev <abochev@gmail.com>
2022-10-15 21:49:13 +0300

    JSX и элементы done

--- task3015/src/element.js
+++ task3015/src/element.js
@@ -2,5 +2,10 @@ import React from 'react';
 import { getUser } from './getUser';
 
 export const element = () => {
-  return null;
+  const user = getUser();
+  const res = <div>
+                <div>Welcome on board</div>
+                <h2>{user.firstName} {user.lastName}</h2>
+              </div>
+  return res;
 };
